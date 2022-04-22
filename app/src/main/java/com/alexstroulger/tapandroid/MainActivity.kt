2f61e29d91311a163077d7d5602d0b47ab154501
astr0-4 <alexstroulger@gmail.com>
2022-04-21 20:54:01 -0700

    f

--- app/src/main/java/com/alexstroulger/tapandroid/MainActivity.kt
+++ app/src/main/java/com/alexstroulger/tapandroid/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.alexstroulger.tapandroid
 
-import android.icu.text.DateFormat.YEAR_ABBR_MONTH
-import android.icu.text.DateFormat.getPatternInstance
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,7 +19,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alexstroulger.tapandroid.ui.theme.TapAndroidTheme
 import com.alexstroulger.tapandroid.ui.theme.Typography
-import java.text.DateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -32,11 +29,32 @@ class MainActivity : ComponentActivity() {
         setContent {
             TapAndroidTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    val steps = listOf(StepSequence(sequence = "shuffle ball change", multiplier = 3))
-                    val dances = listOf(Dance("Dance 1", id = UUID.randomUUID().toString(), steps = steps, createdDate = LocalDate.now()),
-                        Dance("Dance 2", id = UUID.randomUUID().toString(), steps = steps, createdDate = LocalDate.now()),
-                        Dance("Dance 3", id = UUID.randomUUID().toString(), steps = steps, createdDate = LocalDate.now()))
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val steps =
+                        listOf(StepSequence(sequence = "shuffle ball change", multiplier = 3))
+                    val dances = listOf(
+                        Dance(
+                            "Dance 1",
+                            id = UUID.randomUUID().toString(),
+                            steps = steps,
+                            createdDate = LocalDate.now()
+                        ),
+                        Dance(
+                            "Dance 2",
+                            id = UUID.randomUUID().toString(),
+                            steps = steps,
+                            createdDate = LocalDate.now()
+                        ),
+                        Dance(
+                            "Dance 3",
+                            id = UUID.randomUUID().toString(),
+                            steps = steps,
+                            createdDate = LocalDate.now()
+                        )
+                    )
                     DanceList(dances = dances)
                 }
             }
@@ -52,7 +70,11 @@ fun DanceList(dances: List<Dance>) {
             Spacer(Modifier.height(10.0.dp))
             Text(dance.name, style = Typography.h5)
             Spacer(Modifier.height(10.0.dp))
-            Text(dance.createdDate.format(formatter), color = Color.Gray, style = Typography.caption)
+            Text(
+                dance.createdDate.format(formatter),
+                color = Color.Gray,
+                style = Typography.caption
+            )
             Spacer(Modifier.height(10.0.dp))
             Divider(color = Color.Gray)
         })
@@ -64,9 +86,26 @@ fun DanceList(dances: List<Dance>) {
 fun DefaultPreview() {
     TapAndroidTheme {
         val steps = listOf(StepSequence(sequence = "shuffle ball change", multiplier = 3))
-        val dances = listOf(Dance("Dance 1", id = UUID.randomUUID().toString(), steps = steps, createdDate = LocalDate.now()),
-            Dance("Dance 2", id = UUID.randomUUID().toString(), steps = steps, createdDate = LocalDate.now()),
-            Dance("Dance 3", id = UUID.randomUUID().toString(), steps = steps, createdDate = LocalDate.now()))
+        val dances = listOf(
+            Dance(
+                "Dance 1",
+                id = UUID.randomUUID().toString(),
+                steps = steps,
+                createdDate = LocalDate.now()
+            ),
+            Dance(
+                "Dance 2",
+                id = UUID.randomUUID().toString(),
+                steps = steps,
+                createdDate = LocalDate.now()
+            ),
+            Dance(
+                "Dance 3",
+                id = UUID.randomUUID().toString(),
+                steps = steps,
+                createdDate = LocalDate.now()
+            )
+        )
         DanceList(dances = dances)
     }
 }
